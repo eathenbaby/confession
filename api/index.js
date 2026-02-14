79f13318dd45a959fd547cd9b7518cda8a6e3db7
eathenbaby <eathenbaby78@gmail.com>
2026-02-14 19:43:38 +0530

    Add placeholder api/index.js for Vercel and simplify config

---
+++ api/index.js
@@ -0,0 +1,5 @@
+// This file will be replaced during build
+// Placeholder for Vercel deployment
+export default function handler(req, res) {
+  res.status(503).json({ message: 'Build in progress' });
+}
